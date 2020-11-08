88af8bdbbd7a781491ac750be7a06ee8d83ffd8b
ultimateshark <manishpandey8858@gmail.com>
2020-11-08 22:16:03 +0530

    Initial Setup

---
+++ config/server.py
@@ -0,0 +1,5 @@
+serverinfo={
+    DEBUG=True
+    PORT=5000
+    HOST="0.0.0.0"
+}
---
+++ main/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+class Runner:
+    def __init__():
+        self.app = Flask(__name__)
+        self.DEBUG = False
+
+app = Flask(__name__)
+
+def appRunner():
+    app.run()
